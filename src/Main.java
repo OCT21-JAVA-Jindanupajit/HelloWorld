9926bb058b9960f82b86fca43c9f37554b87a200
Krissada Jindanupajit <jindanupajit@gmail.com>
2019-10-21 16:16:54 -0400

    New Changes

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
 public class Main {
 
     public static void main (String [] args) {
+
+        // Printing out Hello World to the console
         System.out.println("Hello World");
+
+        //Declaration of a variable
+        String name;
+
+        //Initialization of a variable
+        name = "Krissada Jindanupajit";
+
+        //Declaration and initialization
+        String newName = "Brad";
+
+        //Reinitializing
+        name = "Sam";
+        System.out.println(name);
     }
 }
